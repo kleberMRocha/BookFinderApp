f8952f0c8c198830d96c18c34bfe6dd2540f2a19
kleber Rocha <kleberrocha2701@gmail.com>
2021-01-04 00:37:11 -0300

    add react-router-dom

--- src/components/BookCard/index.tsx
+++ src/components/BookCard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {useState } from "react";
 import { Container, FaHeartIcon } from "./style";
 
 interface BookCardProps {
@@ -20,7 +20,6 @@ interface BookCardProps {
     };
   };
 }
-
 
 const BookCard: React.FC<BookCardProps> = (Props) => {
 
